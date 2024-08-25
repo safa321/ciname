3ed7a7a1180c3ea64b29c16f29791747f9f189bb
safa321 <safamahdi312@gmail.com>
2024-08-25 17:41:21 +0300

    Add files

--- html/app.js
+++ html/app.js
@@ -82,3 +82,14 @@ function changeVideo(newVideoSrc) {
     videoPlayer.load();
     videoPlayer.play();
 }
+const signUpButton = document.getElementById('signUp');
+const signInButton = document.getElementById('signIn');
+const container = document.getElementById('container');
+
+signUpButton.addEventListener('click', () => {
+	container.classList.add("right-panel-active");
+});
+
+signInButton.addEventListener('click', () => {
+	container.classList.remove("right-panel-active");
+});
